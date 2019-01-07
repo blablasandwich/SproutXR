188c90f97859dd6d833d82e663c044bc411a5869
Jose <contrejo27@gmail.com>
2019-01-07 17:16:45 -0500

    adding login UI

--- Assets/MedievalMath/Scripts/UI/StudentOrTeacher_Canvas.cs
+++ Assets/MedievalMath/Scripts/UI/StudentOrTeacher_Canvas.cs
@@ -10,23 +10,69 @@ public class StudentOrTeacher_Canvas : CanvasNavigation
     [Header("UI References")]
     [SerializeField] private Button studentButton;
     [SerializeField] private Button teacherButton;
+    [SerializeField] private Button parentButton;
+    public GameObject studentProfileCanvas;
+    public GameObject teacherProfileCanvas;
+    public GameObject parentProfileCanvas;
 
     MathController mController;
 
 	private void Start()
 	{
-        if (teacherButton) teacherButton.onClick.AddListener(SchoolSelected);
-        if (studentButton) studentButton.onClick.AddListener(homePressed);
+        if (teacherButton) teacherButton.onClick.AddListener(TeacherSelected);
+        if (studentButton) studentButton.onClick.AddListener(StudentSelected);
+        if (parentButton) parentButton.onClick.AddListener(ParentSelected);
 
     }
 
 
-    void SchoolSelected()
+    void TeacherSelected()
     {
-            GoToNextCanvas();
+        //TODO: Add button selected analytics
+        GoToCustomCanvas("teacher");
     }
-    void homePressed()
+
+    void ParentSelected()
     {
-        GoToNextCanvas();
+        //TODO: Add button selected analytics
+        GoToCustomCanvas("parent");
+    }
+
+    void StudentSelected()
+    {
+        //TODO: Add button selected analytics
+        GoToCustomCanvas("student");
+    }
+
+    void GoToCustomCanvas(string canvasType)
+    {
+        switch (canvasType)
+        {
+            case "teacher":
+                SpawnCanvas(teacherProfileCanvas);
+                break;
+            case "parent":
+                SpawnCanvas(parentProfileCanvas);
+                break;
+            case "student":
+                SpawnCanvas(studentProfileCanvas);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    void SpawnCanvas(GameObject canvas)
+    {
+        GameObject canvasToSpawn = null;
+
+        canvasToSpawn = canvas;
+
+        if (canvasToSpawn)
+            canvasToSpawn = Instantiate(canvasToSpawn);
+
+        Destroy(this.gameObject);
     }
 }
+
