27ee81055a11221f8048d4283f80c5442f262fc9
Felix Lam <blablafoof@gmail.com>
2019-01-07 15:15:12 -0500

    Updated project settings and download settings

---
+++ Assets/Plugins/UnityPurchasing/generated/AppleTangle.cs
@@ -0,0 +1,20 @@
+#if UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE_OSX || UNITY_TVOS
+// WARNING: Do not modify! Generated file.
+
+namespace UnityEngine.Purchasing.Security {
+    public class AppleTangle
+    {
+        private static byte[] data = System.Convert.FromBase64String("anZxbHdqZy8ILgoYHUsaHQ0TX25qd3h3fX9qez58Zz5/cGc+bn9sand4d31/andxcD5fa2p2cWx3amcvAY/FAFlO9RvzQGeaM/UovElSS/IuDxgdSxoUDRRfbm5yez5XcH0wL959LWnpJBkySPXEET8QxKRtB1GrcHo+fXFwendqd3FwbT5xeD5rbXsYLhEYHUsDDR8f4RobLh0fH+EuAwguChgdSxodDRNfbm5yez5McXFqOvz1z6luwRFb/znU73Nm8/mrCQkunBqlLpwdvb4dHB8cHB8cLhMYFxoYDRxLTS8NLg8YHUsaFA0UX25ukW2fftgFRRcxjKzmWlbufiaAC+s0mFaY6RMfHxsbHi58LxUuFxgdS1fGaIEtCnu/aYrXMxwdHx4fvZwfPnF4Pmp2ez5qdntwPn9ubnJ3fX9nPn9tbWtze20+f319e25qf3B9exnyYyedlU0+zSbar6GEURR14TXiMF646VlTYRZALgEYHUsDPRoGLghucns+XXtsand4d31/andxcD5fa6kFo41cOgw02REDqFOCQH3WVZ4JExgXNJhWmOkTHx8bGx4dnB8fHkKrJLPqERAejBWvPwgwassiE8V8CBGDI+01VzYE1uDQq6cQx0ACyNUjbH99and9ez5tan9qe3N7cGptMC6LgGQSulmVRcoIKS3V2hFT0Ap3z0e5GxdiCV5IDwBqzamVPSVZvctxIzh5PpQtdOkTnNHA9b0x5010RXpMe3J3f3B9ez5xcD5qdndtPn17bG5yez5McXFqPl1fLgAJEy4oLiosLShELnwvFS4XGB1LGhgNHEtNLw0oh1IzZqnzkoXC7WmF7GjMaS5R354KNc53WYpoF+DqdZMwXrjpWVNheZEWqj7p1bIyPnFuqCEfLpKpXdFyez5XcH0wLzguOhgdSxoVDQNfbj5dXy6cHzwuExgXNJhWmOkTHx8fAZudmwWHI1kp7LeFXpAyyq+ODMYrLC8qLi0oRAkTLSsuLC4nLC8qLrW9b4xZTUvfsTFfrebl/W7T+L1StsJgPCvUO8vHEch1yrw6PQ/pv7IbHh2cHxEeLpwfFBycHx8e+o+3FzI+fXtsand4d31/ans+bnFyd31n1wds60MQy2FBhew7HaRLkVNDE+8WQC6cHw8YHUsDPhqcHxYunB8aLhY1GB8bGxkcHwgAdmpqbm0kMTFpeis9C1ULRwOtiunogoDRTqTfRk5hX7aG58/UeII6dQ/OvaX6BTTdAVtgAVJ1Tohfl9pqfBUOnV+ZLZSfPn9wej59e2xqd3h3fX9qd3FwPm4xLp/dGBY1GB8bGxkcHC6fqASfrWlpMH9ubnJ7MH1xczF/bm5ye31/xyhh35lLx7mHpyxc5cbLb4Bgv0yVB5fA51Vy6xm1PC4c9gYg5k4XzRgdSwMQGggaCjXOd1mKaBfg6nWTnB8eGBc0mFaY6X16Gx8un+wuNBg4LjoYHUsaFQ0DX25ucns+XXtsamQunB9oLhAYHUsDER8f4RoaHRwffHJ7Pm1qf3B6f2x6Pmp7bHNtPn+g6m2F8Mx6EdVnUSrGvCDnZuF11q8uRvJEGiySdq2RA8B7beF5QHuiTrSUy8T64s4XGSmua2s/");
+        private static int[] order = new int[] { 5,39,11,15,9,57,43,29,12,53,57,11,55,39,27,41,40,51,31,53,59,47,23,23,46,31,52,56,38,43,55,48,32,47,41,39,49,43,46,54,56,44,53,47,54,59,51,48,59,59,59,58,56,55,54,57,58,57,59,59,60 };
+        private static int key = 30;
+
+        public static readonly bool IsPopulated = true;
+
+        public static byte[] Data() {
+        	if (IsPopulated == false)
+        		return null;
+            return Obfuscator.DeObfuscate(data, order, key);
+        }
+    }
+}
+#endif
