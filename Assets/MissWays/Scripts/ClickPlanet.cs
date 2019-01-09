d327c15899592eeee98533123a2a12472b97a447
Patrick <35389689+PatAtUCF@users.noreply.github.com>
2019-01-09 13:35:14 -0500

    updated clicking Missways on dash
    temp/ jump to planet scanning

--- Assets/MissWays/Scripts/ClickPlanet.cs
+++ Assets/MissWays/Scripts/ClickPlanet.cs
@@ -2,6 +2,8 @@
 using UnityEngine.SceneManagement;
 public class ClickPlanet : MonoBehaviour
 {
+    public GameObject QuestionPrefab;
+
     void Update()
     {
         if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
@@ -14,12 +16,16 @@ public class ClickPlanet : MonoBehaviour
                 {
                     //StaticVars.UIHeaderText.SetText("ABC");
                     //StaticVars.UI.transform.GetChild(1).gameObject.SetActive(true);
-                    SceneManager.LoadScene(1);
-                    GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0, 1);
+                    //  SceneManager.LoadScene(1);
+                    //  GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0, 1);
+                    Instantiate(QuestionPrefab, Vector3.zero, Quaternion.identity);
+
                 }
                 else if(raycastHit.collider.CompareTag("Clickable")) {
-                    SceneManager.LoadScene(1);
-                    GetComponent<MeshRenderer>().material.color = new Color(0, 1, 0, 1);
+                   // SceneManager.LoadScene(1);
+                   // GetComponent<MeshRenderer>().material.color = new Color(0, 1, 0, 1);
+
+
                 }
 
                 if (raycastHit.collider.CompareTag("Floor"))
