da67266f2c21934170ec1e7b8f90337a8f0aeb6c
PatAtUCF <35389689+PatAtUCF@users.noreply.github.com>
2019-02-04 17:40:19 -0500

    made video work, tv shows up, implemented vuforia

---
+++ Assets/MissWays/Scripts/ExtraImageTargetBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExtraImageTargetBehavior : DefaultTrackableEventHandler
+{
+    /*public Text debugText;
+    void OnTrackingFound()
+    {
+        debugText.text += "Tracking Found";
+    }
+
+    void OnTrackingLost()
+    {
+       debugText.text += "Tracking Found";
+
+    }*/
+}
