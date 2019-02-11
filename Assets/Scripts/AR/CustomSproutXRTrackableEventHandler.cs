9d1937ef22371c2106f2a842a422c63a68c93542
Eugenio <eugenio@lucernastudios.com>
2019-02-11 14:56:38 -0500

    changed handler name

--- Assets/Scripts/AR/CustomSproutXRTrackableEventHandler.cs
+++ Assets/Scripts/AR/CustomSproutXRTrackableEventHandler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurnOffOnLeave : DefaultTrackableEventHandler
+public class CustomSproutXRTrackableEventHandler : DefaultTrackableEventHandler
 {
     public int VideoToPlay;
 
