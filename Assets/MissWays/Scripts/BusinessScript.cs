b9563ba9611119f0cede1e0f274bcacb9a2bf2f4
Dylan Rodrigues <chefcro0225@gmail.com>
2019-02-12 13:33:04 -0500

    - Added new card to Database - Created BusinessScript, this script makes and pulls an object from the array to display when the card is chosen - Created associated scan object in scene

---
+++ Assets/MissWays/Scripts/BusinessScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomArrayExtensions;
+
+namespace CustomArrayExtensions
+{
+    public static class ArrayExtensions
+    {
+        public static T GetRandom<T>(this T[] array)
+        {
+            return array[Random.Range(0, array.Length)];
+        }
+    }
+}
+
+public class BusinessScript : MonoBehaviour
+{
+    [SerializeField] public GameObject[] myPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject selectedPrefab = myPrefabs.GetRandom();
+
+        Instantiate(selectedPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
