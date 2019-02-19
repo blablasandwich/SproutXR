5398c3ef6b1ac58cbf3b13b874cee302c2eba17c
Dylan Rodrigues <chefcro0225@gmail.com>
2019-02-19 15:01:32 -0500

    Business Card Completed Enemies from Kells level spawn from castle, shortly afterwards gravity effectsthem and they can be slid off of the business card in AR

--- Assets/MissWays/Scripts/BusinessScript.cs
+++ Assets/MissWays/Scripts/BusinessScript.cs
@@ -1,33 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using CustomArrayExtensions;
-
-namespace CustomArrayExtensions
-{
-    public static class ArrayExtensions
-    {
-        public static T GetRandom<T>(this T[] array)
-        {
-            return array[Random.Range(0, array.Length)];
-        }
-    }
-}
 
 public class BusinessScript : MonoBehaviour
 {
-    [SerializeField] public GameObject[] myPrefabs;
+    public GameObject[] myPrefabs;
+
+    int index;
+
+    public float Timer = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        GameObject selectedPrefab = myPrefabs.GetRandom();
-
-        Instantiate(selectedPrefab, transform.position, transform.rotation);
+        Debug.Log("I have started");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Timer -= 1 * Time.deltaTime;
+        if (Timer <= 0)
+        {
+            index = Random.Range(0, myPrefabs.Length);
+
+            GameObject selectedPrefab = myPrefabs[index];
+
+            GameObject character = Instantiate(selectedPrefab, transform.position, transform.rotation);
+
+            character.transform.parent = GameObject.Find("Castle").transform;
+
+            Timer = .5f;
+        }
     }
+
+    /*
+    private IEnumerator startLate()
+    {
+        yield return new WaitForSeconds(1f);
+
+        StartCoroutine(SpawnDudes());
+    }
+
+    private IEnumerator SpawnDudes()
+    {
+        Debug.Log("It's WORKING!");
+
+        index = Random.Range(0, myPrefabs.Length);
+
+        GameObject selectedPrefab = myPrefabs[index];
+
+        yield return new WaitForSeconds(3f);
+
+        Instantiate(selectedPrefab, transform.position, transform.rotation);
+    }
+    */
 }
