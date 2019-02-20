1ce262c173e5552d353b45aac16c9afd7ba8b9d0
Pat <35389689+PatAtUCF@users.noreply.github.com>
2019-02-20 10:45:15 -0500

    anim prefab update, just add new anims to the animationbase controller and give it a trigger/add it to the enum switch statment, re-enabled main menu, pushing removed files from the past to the repo so the repo knows they are gone.

---
+++ Assets/MissWays/Scripts/AnimPicker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimPicker : MonoBehaviour
+{
+
+    public enum AnimationToPlay { None, Scale };
+
+    public AnimationToPlay AnimToPlay;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (AnimToPlay)
+        {
+            case AnimationToPlay.None:
+                anim.SetTrigger("None");
+                break;
+            case AnimationToPlay.Scale:
+                anim.SetTrigger("Scale");
+                break;
+            default:
+                anim.SetTrigger("None");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
