2b121a206f70b317ba8803070596738ae7266af7
Pat <35389689+PatAtUCF@users.noreply.github.com>
2019-02-20 11:37:48 -0500

    added roof removal on click

---
+++ Assets/OpenRoof.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenRoof : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        anim.StopPlayback();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {                
+            if(raycastHit.Equals(this))
+                {
+                    anim.SetTrigger("Open");
+                }
+            }
+        }
+}
+}
