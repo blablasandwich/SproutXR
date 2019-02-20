72d1710ebb5ed6b9b07dd0830c2b29cfd6bfbe52
Pat <35389689+PatAtUCF@users.noreply.github.com>
2019-02-20 13:44:09 -0500

    added student selection prefab, just need to put it in place, and replace one liners user for generation and login payload

---
+++ Assets/GridBasedStudentLogIn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridBasedStudentLogIn : MonoBehaviour
+{
+    public GameObject studentProfilePicPrefab;
+    public GameObject studentCardHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            GameObject currentStudent = Instantiate(studentProfilePicPrefab, studentCardHolder.transform);
+            GridStudentButtonInfo info = currentStudent.GetComponent<GridStudentButtonInfo>();
+
+
+            Color c = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+            info.pic.color = c;
+
+            string fakeName = "";
+
+            for (int x = 0; x < 5; x++)
+            {
+                char n = (char)('A' + Random.Range(0, 26));
+                fakeName += n;
+            }
+
+            info.studentName.text = fakeName;
+            info.name = fakeName;
+        }
+    }
+
+ 
+
+}
---
+++ Assets/GridStudentButtonInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class GridStudentButtonInfo : MonoBehaviour
+{
+    public Image pic;
+    public TextMeshProUGUI studentName;
+    public Button btn;
+    // Start is called before the first frame update
+
+
+    public void PressBtn()
+    {
+        Debug.Log(studentName.text);
+    }
+}
