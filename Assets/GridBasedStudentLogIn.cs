de1c62c95d1379c601ff6764766729ffd1189a0a
Pat <35389689+PatAtUCF@users.noreply.github.com>
2019-02-21 11:43:29 -0500

    internet is back, uplaoding the json readable version for user selection

--- Assets/GridBasedStudentLogIn.cs
+++ Assets/GridBasedStudentLogIn.cs
@@ -2,37 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class GridBasedStudentLogIn : MonoBehaviour
 {
     public GameObject studentProfilePicPrefab;
     public GameObject studentCardHolder;
+    List<MyObject> JsonArry;
 
-    // Start is called before the first frame update
-    void Start()
+        // Start is called before the first frame update
+     void Start()
     {
-        for (int y = 0; y < 5; y++)
+        StartCoroutine("CheckDataBase");
+    }
+
+    void ParseJsonToObject(string json)
+    {
+        var wrappedjsonArray = JsonUtility.FromJson<MyWrapper>(json);
+        JsonArry = wrappedjsonArray.users;
+    }
+
+    [System.Serializable]
+    private class MyWrapper
+    {
+        public List<MyObject> users;
+    }
+
+    [System.Serializable]
+    private class MyObject
+    {
+        public string username;
+    }
+
+    void stuff(UnityWebRequest w)
+    {
+        ParseJsonToObject(w.downloadHandler.text);
+
+    foreach(MyObject obj in JsonArry)
         {
             GameObject currentStudent = Instantiate(studentProfilePicPrefab, studentCardHolder.transform);
             GridStudentButtonInfo info = currentStudent.GetComponent<GridStudentButtonInfo>();
 
-
+            //set icon color
             Color c = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
             info.pic.color = c;
 
-            string fakeName = "";
-
-            for (int x = 0; x < 5; x++)
-            {
-                char n = (char)('A' + Random.Range(0, 26));
-                fakeName += n;
-            }
-
-            info.studentName.text = fakeName;
-            info.name = fakeName;
+            //set gameobjname and user name
+            info.studentName.text = obj.username;
+            info.name = obj.username;
         }
     }
 
- 
+    public IEnumerator CheckDataBase()
+    {
+        using (UnityWebRequest w = UnityWebRequest.Get("http://sproutxr-api-dev.herokuapp.com/api/classroom/15/user"))
+        {
+            yield return w.SendWebRequest();
 
+            if (w.isNetworkError || w.isHttpError)
+            {
+                Debug.Log("Error: " + w.error);
+            }
+            else
+            {
+                Debug.Log(w.downloadHandler.text);
+
+                stuff(w);
+            }
+        }
+    }
 }
