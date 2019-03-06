7084a6e406b91feff569e4c9b85d37660c1d7d85
Pat <35389689+PatAtUCF@users.noreply.github.com>
2019-03-05 20:17:42 -0500

    Revert "pushing classroom id main menu currently only classroom id 15 works"
    This reverts commit 76b27bed3aec8a6b131834328beb9bf8037d8d3d.

--- Assets/GridBasedStudentLogIn.cs
+++ Assets/GridBasedStudentLogIn.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 
 public class GridBasedStudentLogIn : MonoBehaviour
@@ -9,13 +9,12 @@ public class GridBasedStudentLogIn : MonoBehaviour
     public GameObject studentProfilePicPrefab;
     public GameObject studentCardHolder;
     List<MyObject> JsonArry;
-    public TextMeshProUGUI URLID;
-    public void ClickBtn()
+
+        // Start is called before the first frame update
+     void Start()
     {
-      //  string id = inputid.text;
         StartCoroutine("CheckDataBase");
     }
-
 
     void ParseJsonToObject(string json)
     {
@@ -39,6 +38,7 @@ public class GridBasedStudentLogIn : MonoBehaviour
     {
         ParseJsonToObject(w.downloadHandler.text);
 
+        for (int i = 0; i < 10; i++) {
             foreach (MyObject obj in JsonArry)
             {
                 GameObject currentStudent = Instantiate(studentProfilePicPrefab, studentCardHolder.transform);
@@ -53,67 +53,16 @@ public class GridBasedStudentLogIn : MonoBehaviour
                 info.name = obj.username;
             }
         }
+    }
 
     public IEnumerator CheckDataBase()
     {
-        switch (URLID.text)
-        {
-            case "15":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/15/user";
-                break;
-            case "14":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/14/user";
-                break;
-            case "13":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/13/user";
-                break;
-            case "12":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/12/user";
-                break;
-            case "11":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/11/user";
-                break;
-            case "10":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/10/user";
-                break;
-            case "9":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/9/user";
-                break;
-            case "8":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/8/user";
-                break;
-            case "7":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/7/user";
-                break;
-            case "6":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/6/user";
-                break;
-            case "5":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/5/user";
-                break;
-            case "4":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/4/user";
-                break;
-            case "3":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/3/user";
-                break;
-            case "2":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/2/user";
-                break;
-            case "1":
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/1/user";
-                break;
-            default:
-                URLID.text = "http://sproutxr-api-dev.herokuapp.com/api/classroom/0/user";
-                break;
-        }
-        using (UnityWebRequest w = UnityWebRequest.Get(URLID.text))
+        using (UnityWebRequest w = UnityWebRequest.Get("http://sproutxr-api-dev.herokuapp.com/api/classroom/15/user"))
         {
             yield return w.SendWebRequest();
 
             if (w.isNetworkError || w.isHttpError)
             {
-                Debug.Log(URLID.text);
                 Debug.Log("Error: " + w.error);
             }
             else
