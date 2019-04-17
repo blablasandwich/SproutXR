110c3ac78d10f6a19983186b512f20346e153177
Pat <35389689+PatAtUCF@users.noreply.github.com>
2019-04-17 14:02:09 -0400

    vocab mat edit on click and light on / off

---
+++ Assets/ClickVocab.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickVocab : MonoBehaviour
+{
+    private string NAME;
+    private Material mat;
+    private bool b;
+    private Color on = new Color(0.08f, 0.71f, 0, 1);
+    private Color off = new Color(0.08f, 0.3f, 0, 1);
+    // Start is called before the first frame update
+    void Start()
+    {
+        NAME = gameObject.name;
+        mat = gameObject.transform.GetChild(1).gameObject.GetComponent<Renderer>().material;
+        b = false;
+        mat.SetColor("_EmissionColor", off);
+    }
+
+    public void toggleHighLight()
+    {
+        b = !b;
+        if (b)
+        {
+            turnOff();
+        }
+        else
+        {
+            turnOn();
+        }
+    }
+
+    void turnOn()
+    {
+        mat.SetColor("_EmissionColor", on);
+        lightbulb.toggleVocabLight();
+    }
+
+    void turnOff()
+    {
+        mat.SetColor("_EmissionColor", off);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                if (raycastHit.collider.gameObject.name == NAME)
+                {
+                    toggleHighLight();
+                }
+
+            }
+        }
+    }
+}
---
+++ Assets/lightbulb.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightbulb : MonoBehaviour
+{
+    private Material[] mats;
+    private static Material bulb;
+    private static GameObject lightSource;
+    private static Color on = new Color(.75f, .75f, .75f, 1f);
+    private static Color off = new Color(0, 0, 0, 0);
+    private static bool b = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightSource = gameObject.transform.GetChild(0).gameObject;
+        mats = gameObject.transform.GetChild(1).gameObject.GetComponent<Renderer>().materials;
+        bulb = mats[1];
+        toggleVocabLight();
+    }
+
+
+    public static void toggleVocabLight()
+    {
+        b = !b;
+        if (b)
+        {
+            turnOff();
+        }
+        else
+        {
+            turnOn();
+        }
+    }
+
+    static void turnOn()
+    {
+        bulb.SetColor("_EmissionColor", on);
+        lightSource.SetActive(true);
+    }
+
+    static void turnOff()
+    {
+        bulb.SetColor("_EmissionColor", off);
+        lightSource.SetActive(false);
+    }
+
+}
